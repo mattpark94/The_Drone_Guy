ce89f80991f3b838958d77d921558f7eb244295d
mattpark94 <matthew.park.94@gmail.com>
2023-09-23 09:54:31 +1200

    Converting the HTML to Node App

---
+++ old_files/config/config.js
@@ -0,0 +1,7 @@
+ MySQL Database Configuration
+MYSQL_HOST = 'localhost'  // Replace with your MySQL host address if it's not running locally
+MYSQL_USER = 'root'       // Replace with your MySQL username
+MYSQL_PASSWORD = ''       // Replace with your MySQL password if you have one
+MYSQL_DB = 'your_database_name'  // Replace with the name of your MySQL database
+
+# You can add additional configurations as needed
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var db=require('./dbConfig');
+ 
+var app = express();
+ 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+ 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, next) {
+	res.render('home', { title: 'Home' });
+});
+
+app.listen(3000);
+console.log('Node app is running on port 3000');
