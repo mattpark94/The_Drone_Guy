f163abf41e3e9f1b54b9fb75dca6f85c3a483c3f
mattpark94 <matthew.park.94@gmail.com>
2023-09-23 16:10:41 +1200

    Website Working
    Got the website working.

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 var express = require('express');
 var path = require('path');
-var db=require('./dbConfig');
- 
+var db = require('./dbConfig');
+
 var app = express();
- 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
- 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define the route for the home page
 app.get('/', function(req, res, next) {
-	res.render('home', { title: 'Home' });
+  res.render('home', { title: 'Home' });
 });
 
-app.listen(3000);
-console.log('Node app is running on port 3000');
+// Define the route for the about page
+app.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+app.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+app.listen(3000, function () {
+  console.log('Node app is running on port 3000');
+});
