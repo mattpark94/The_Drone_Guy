595a960ac15e3fd75db732b63961425c7f1917c1
mattpark94 <matthew.park.94@gmail.com>
2023-11-09 22:35:38 +1300

    Added a Updated Booking form on Admin_book
    Added a Updated Booking form on Admin_book

--- routes/admin.js
+++ routes/admin.js
@@ -24,7 +24,7 @@ router.get('/admin_users', function (req, res) {
         connection.query("SELECT * FROM drone_users", function (err, data) {
             if (err) {
                 throw err;
-              }
+            }
 
             res.render('admin/admin_users', { title: 'Admin Users Page', data: data });
         });
@@ -39,11 +39,11 @@ router.get('/admin_book', function (req, res) {
     if (req.session.user && req.session.user.user_type === 'admin') {
         // User is authenticated and is an admin
 
-        // Retrieve user data from the database
+        // Retrieve booking data from the database
         connection.query("SELECT * FROM drone_booking", function (err, data) {
             if (err) {
                 throw err;
-              }
+            }
 
             res.render('admin/admin_book', { title: 'Admin Booking Page', data: data });
         });
@@ -53,28 +53,78 @@ router.get('/admin_book', function (req, res) {
     }
 });
 
-
-// Add a new POST route for adding bookings
+// Admin adding bookings
 router.post('/admin_book', function (req, res) {
     if (req.session.user && req.session.user.user_type === 'admin') {
         // User is authenticated and is an admin
 
         // Retrieve the booking data from the form
         const { date, time, duration } = req.body;
+        const bookingStatus = 'available'; // Set the booking status to 'available'
 
-        // Insert the new booking into the database
-        connection.query("INSERT INTO drone_booking (booking_date, booking_time, booking_duration) VALUES (?, ?, ?)", [date, time, duration], function (err, result) {
-            if (err) {
-                throw err;
-              }
-
-            // Redirect to the admin_book page to show the updated list of bookings
-            res.redirect('/admin/admin_book');
-        });
+        // Insert the new booking into the database with 'available' status
+        connection.query(
+            "INSERT INTO drone_booking (booking_date, booking_time, booking_duration, booking_status) VALUES (?, ?, ?, ?)",
+            [date, time, duration, bookingStatus],
+            function (err, result) {
+                if (err) {
+                    throw err;
+                } else {
+                    // Redirect to the admin_book page to show the updated list of bookings
+                    res.redirect('/admin/admin_book');
+                }
+            }
+        );
     } else {
         // Handle unauthorized access
         res.redirect('/login');
     }
 });
+
+
+
+// Route to handle deleting bookings
+router.post('/delete-booking', function (req, res) {
+    const bookingId = req.body.booking_id;
+
+    // Delete the booking from the database based on the booking ID
+    connection.query("DELETE FROM drone_booking WHERE booking_id = ?", [bookingId], function (err, result) {
+        if (err) {
+            throw err;
+        }
+
+        // Redirect back to the admin_book page
+        res.redirect('/admin/admin_book');
+    });
+});
+
+
+// Route to handle editing bookings
+router.post('/edit-booking', function (req, res) {
+    if (req.session.user && req.session.user.user_type === 'admin') {
+        // User is authenticated and is an admin
+
+        // Retrieve the booking data from the form
+        const { booking_id, date, time, booking_status, duration } = req.body;
+
+        // Update the booking in the database
+        connection.query(
+            "UPDATE drone_booking SET booking_date=?, booking_time=?, booking_status=?, booking_duration=? WHERE booking_id=?",
+            [date, time, booking_status, duration, booking_id],
+            function (err, result) {
+                if (err) {
+                    throw err;
+                } else {
+                    // Redirect to the admin_book page to show the updated list of bookings
+                    res.redirect('/admin/admin_book');
+                }
+            }
+        );
+    } else {
+        // Handle unauthorized access
+        res.redirect('/login');
+    }
+});
+
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -14,11 +14,11 @@ router.get('/user_landing', function (req, res) {
   }
 });
 
-// Import user booked data into a table
+// Import user booking data into a table
 router.get('/user_landing', function (req, res) {
   if (req.session.user && req.session.user.user_type === 'user') {
     // Retrieve user data from the database
-    connection.query('SELECT * FROM drone_booked WHERE user_id = ?', [req.session.user.user_id], function (err, data) {
+    connection.query('SELECT * FROM drone_booking WHERE user_id = ?', [req.session.user.user_id], function (err, data) {
       if (err) {
         // Throw the error instead of rendering an error page
         console.error(err);
@@ -42,7 +42,7 @@ router.get('/user_book', function (req, res) {
     connection.query('SELECT * FROM drone_booking', function (err, data) {
       if (err) throw err;
 
-      res.render('user/user_book', { title: 'User Booking Page', data: data });
+      res.render('user/user_book', { title: 'User Booking Page', data: data, user: req.session.user }); // Pass the user data to the template
     });
   } else {
     // Handle unauthorized access
@@ -51,30 +51,31 @@ router.get('/user_book', function (req, res) {
 });
 
 
-// Handle the booking request
+
 router.post('/book_drone', function (req, res) {
   if (req.session.user && req.session.user.user_type === 'user') {
-    // Get the booking details from the request
-    const { booking_date, booking_time } = req.body;
+      // Get the booking details from the request
+      const { booking_date, booking_time, user_id, booking_status } = req.body;
 
-    // Insert the booking details into the drone_booked table
-    connection.query(
-      'INSERT INTO drone_booked (booked_first_name, booked_last_name, booked_email, booked_date, booked_time) VALUES (?, ?, ?, ?, ?)',
-      [req.session.user_first_name, req.session.user_last_name, req.session.user_email, booking_date, booking_time],
-      function (err, result) {
-        if (err) {
-          throw err;
-        }
+      // Update the booking details in the drone_booking table
+      connection.query(
+          'UPDATE drone_booking SET booking_user_id = ?, booking_status = ? WHERE booking_date = ? AND booking_time = ?',
+          [user_id, booking_status, booking_date, booking_time],
+          function (err, result) {
+              if (err) {
+                  throw err;
+              }
 
-        // Redirect to the user landing page or display a success message
-        res.redirect('/user/user_landing');
-      }
-    );
+              // Redirect to the user landing page or display a success message
+              res.redirect('/user/user_landing');
+          }
+      );
   } else {
-    // Handle unauthorized access
-    res.redirect('/login');
+      // Handle unauthorized access
+      res.redirect('/login');
   }
 });
+
 
 module.exports = router;
 
